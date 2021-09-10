4b1e1ae5214f22a7fe3f15df84746912176975e2
DSoutter <duncan.soutter@gmail.com>
2021-09-10 15:55:21 +0100

    boilerplate for server.js done

---
+++ server/helpers/create_router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+
+const createRouter = function(collection) {
+
+
+    return router
+};
+
+module.exports = createRouter
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongo').MongoClient;
+const createRouter = require('./helpers/create_router');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+  if(err){
+    console.log(err);
+  }
+
+  const db = client.db('connect_four');
+  const gamesCollection = db.collection('games');
+  const gamesRouter = createRouter(gamesCollection)
+  app.use('/api/games', gamesRouter);
+
+  app.listen(5000, function(){
+    console.log(`app listening on port ${this.address().port}`);
+  })
+})
