9dfcf43352cb2a7f3ed835a7d6bcd1e91abed9e4
DSoutter <duncan.soutter@gmail.com>
2021-09-11 21:22:36 +0100

    started components and containers

---
+++ client/src/containers/ScreenContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ScreenContainer = () => {
+
+
+
+    return (
+        <>
+        <HeaderComponent/>
+        <h1>I am the screen container</h1>
+        <GameBoardComponent/>
+        <ScoreComponent/>
+        <FooterComponent/>
+        </>
+    )
+
+}
+
+export default ScreenContainer
