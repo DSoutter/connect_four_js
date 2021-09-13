e7dc0faeae31b9e41085a609f9132f670fdf62c4
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-13 12:14:50 +0100

    added logic folder, Cell class

---
+++ js_logic/Cell.js
@@ -0,0 +1,21 @@
+class Cell {
+    constructor(id, player = null, winning = false) {
+        this.id = id,
+        this.player = player,       
+        this.winning = winning
+    }
+
+    isWinning() {
+        //how is this different from setwin?
+        this.winning = true
+    }
+
+    claim(player) {
+        //set the cell to the players colour
+        this.player = player
+    }
+
+    setWin() {
+        //do something to the cells that make up the winning connect 4
+    }
+}
