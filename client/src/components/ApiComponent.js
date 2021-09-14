df39658aee7d1fc3da61aac3579144ff1e8af63b
DSoutter <duncan.soutter@gmail.com>
2021-09-14 12:10:17 +0100

    working on api best method

--- client/src/components/ApiComponent.js
+++ client/src/components/ApiComponent.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 
 const ApiComponent = () => {
 
-    const [computerMoves, setComputerMoves] = useState(null)
+    const [computerMoves, setComputerMoves] = useState([])
 
     const apiResponse = function () {
         fetch(`http://kevinalbs.com/connect4/back-end/index.php/getMoves?board_data=0000000000000000020000001200000210000021001012100&player=2`)
@@ -17,25 +17,34 @@ const ApiComponent = () => {
         apiResponse()}
         ,[])
 
-    if (!computerMoves) {
-        return <p>Loading...</p>
-    }
+    // if (computerMoves.length) {
+    //     return <p>Loading...</p>
+    // }
 
 
 
     // find the highest value in the array of 7...
-    const bestMove = (computerMoves) => {
-        let currentBest = 1+ parseInt(computerMoves[0][0])
-        console.log(currentBest)
-        return currentBest
-    }
+    // const bestMove = (computerMoves) => {
+    //     if (computerMoves.length >0) {
+    //     let currentBest = (computerMoves[0][0])
+    //     console.log(currentBest)
+    //     for (let i=1; computerMoves.length; i++){
+    //         if (computerMoves[i][1] > currentBest) {
+    //             currentBest = computerMoves[i][1]
+    //         }
+    //     }
+    //     return currentBest
+    // } else {
+    //     return null
+    // }
+    // }
     
-    bestMove(computerMoves)
+    // bestMove(computerMoves)
 
     return (
         <>
         {/* we want to display the max index 1 (value) of the seven arrays */}
-        <p>The best move is column {bestMove(computerMoves)}</p>
+        <p>The best move is column {(computerMoves)}</p>
         </>
     )
 }
